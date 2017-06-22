a1c826c9f2723613d32dd670e95fe92e32bac3df
Richard Hull <rm_hull@yahoo.co.uk>
2017-06-22 08:33:17 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
     :dev {
       :global-vars {*warn-on-reflection* true}
       :plugins [
-        [lein-codox "0.10.1"]
+        [lein-codox "0.10.3"]
         [lein-cljfmt "0.5.6"]
         [lein-cloverage "1.0.9"]]
       :dependencies [
